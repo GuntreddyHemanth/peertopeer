bd882cafc2c58a1ee38a21196544dc0ef9d9cbe9
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2025-06-27 19:51:09 +0530

    /

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 // import { useState } from 'react'
+
 import './App.css'
-import { Route, BrowserRouter, Routes } from 'react-router-dom'
+import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'
 import { Sender } from './components/Sender'
 import { Receiver } from './components/Receiver'
 
@@ -8,6 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/sender" replace />} />
         <Route path="/sender" element={<Sender/>} />
         <Route path="/receiver" element={<Receiver/>} />
       </Routes>
