3090d3934f394c5c2440701909a0101cbfddac14
GuntreddyHemanth <guntreddyhemanth@gmail.com>
2025-06-27 20:00:59 +0530

    vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // import { useState } from 'react'
 
 import './App.css'
-import { Route, BrowserRouter, Routes, Navigate } from 'react-router-dom'
+import { Route, BrowserRouter, Routes} from 'react-router-dom'
 import { Sender } from './components/Sender'
 import { Receiver } from './components/Receiver'
 
@@ -9,7 +9,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/sender" replace />} />
         <Route path="/sender" element={<Sender/>} />
         <Route path="/receiver" element={<Receiver/>} />
       </Routes>
